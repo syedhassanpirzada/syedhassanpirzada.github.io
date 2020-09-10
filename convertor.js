5da80ada5b07a0acdb2c4db3a7ed68f035a9a74e
Pie <01-134182-088@student.bahria.edu.pk>
2020-09-10 14:15:19 +0500

    Adding files

---
+++ convertor.js
@@ -0,0 +1,6 @@
+function LengthConverter(valNum) {
+  document.getElementById("outputMeters").innerHTML = valNum / 100;
+}
+function weightConverter(valNum) {
+  document.getElementById("outputKG").innerHTML = valNum / 1000;
+}
