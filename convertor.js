a8d151c395a43ea3ba360da4b111c34cca4ea49c
syedhassanpirzada <hassanpirzada2121@gmail.com>
2020-09-10 20:14:07 +0500

    Comments

--- convertor.js
+++ convertor.js
@@ -1,13 +1,16 @@
+//Meters to centimeters
 function LengthConverter(valNum) {
   document.getElementById("outputMeters").innerHTML = valNum / 100;
 }
+//Grams to Kilograms
 function weightConverter1(valNum) {
   document.getElementById("outputKG").innerHTML = valNum / 1000;
 }
-
+//Kilogram to gram
 function weightConverter2(valNum) {
   document.getElementById("outputG").innerHTML = valNum * 1000;
 }
+//Meter to Centimeter
 function lengthConverter2(valNum) {
   document.getElementById("outputCentiMeters").innerHTML = valNum * 1000;
 }
